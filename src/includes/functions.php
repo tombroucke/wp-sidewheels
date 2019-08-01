228dae6e92063a194c2a8eb711015d5101299f1c
Tom Broucke <tom@tombroucke.be>
2019-08-01 21:20:52 +0200

    updated twig support

--- src/includes/functions.php
+++ src/includes/functions.php
@@ -1,4 +1,6 @@
 <?php
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 use \SideWheels\WP_Sidewheels;
 
 if( !function_exists('wp_sidewheels_trigger_404') ) {
@@ -23,5 +25,20 @@ if(!function_exists('wp_sidewheels')) {
 	function wp_sidewheels()
 	{
 		return WP_Sidewheels::get_instance();
+	}
+}
+
+if(!function_exists('wp_sidewheels')) {
+	/**
+	 * [wp_sidewheels_render_template description]
+	 * @param  string $template template path/name.twig
+	 * @param  array $args template args
+	 */
+	function wp_sidewheels_render_template($template, $args)
+	{
+		$loader = new FilesystemLoader(wp_sidewheels()->settings()->get('templates'));
+		$twig = new Environment($loader);
+
+		echo $twig->render($template, $args);
 	}
 }
